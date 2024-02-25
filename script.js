01ebcf2efd2ce847b8b7bb416d8eecaaa08038d6
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-02-25 13:12:54 -0500

    Add functions for add, subtract, multiply, divide
    Functions will take two arguments and run the operation.

---
+++ script.js
@@ -0,0 +1,16 @@
+const add = function (arg1, arg2) {
+    return arg1 + arg2;
+};
+
+const subtract = function (arg1, arg2) {
+    return arg1 - arg2;
+};
+
+const multiply = function (arg1, arg2) {
+    return arg1 * arg2;
+};
+
+const divide = function (arg1, arg2) {
+    return arg1 / arg2;
+};
+
