f99407d0d441d26dddc80388ff3015a6d9058a76
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-02-25 13:15:07 -0500

    Add function to operate
    Declared two variables for the numbers to operate on. Declared another
    variable to be a string for an operator. operate() will create a string
    that forms an equation using the three aforementioned variables.

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ const divide = function (arg1, arg2) {
     return arg1 / arg2;
 };
 
+let num1 = 0;
+let num2 = 0;
+
+// Division character for copy and pasting: ÷
+let operator = '÷';
+
+const operate = function () {
+    let operation = '';
+
+    if (operator === '+') {
+        operation = add(num1, num2);
+    } else if (operator === '-') {
+        operation = subtract(num1, num2);
+    } else if (operator === 'x') {
+        operation = multiply(num1, num2);
+    } else if (operator === '÷') {
+        operation = divide(num1, num2);
+    }
+
+    return `${num1} ${operator} ${num2} = ${operation}`;
+}
+
+console.log(operate());
