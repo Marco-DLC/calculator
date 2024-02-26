91a6d8c0cb111aaf9a83039bbede6a4a4f890e2a
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-02-26 18:38:27 -0500

    Add calculator operation functionality
    User can now enter a number, operator, then a second number and the
    calculator will display the correct answer upon user pressing the equals
    button. Functions were added so that the variables for the numbers and
    the operator are updated when user selects the corresponding buttons.
    Users cannot enter more than one operator, enter an operator before
    inputting a first number, enter a second number before the operator is
    entered or enter an operator after the operation is complete.

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-const add = function (arg1, arg2) {
+function add(arg1, arg2) {
     return arg1 + arg2;
 };
 
-const subtract = function (arg1, arg2) {
+function subtract(arg1, arg2) {
     return arg1 - arg2;
 };
 
-const multiply = function (arg1, arg2) {
+function multiply(arg1, arg2) {
     return arg1 * arg2;
 };
 
-const divide = function (arg1, arg2) {
+function divide(arg1, arg2) {
     return arg1 / arg2;
 };
 
-let num1 = 0;
-let num2 = 0;
-
-// Division character for copy and pasting: ÷
-let operator = '';
-
-const operate = function () {
+function operate() {
     let operation = '';
 
     if (operator === '+') {
@@ -32,30 +26,64 @@ const operate = function () {
     } else if (operator === '÷') {
         operation = divide(num1, num2);
     }
-
-    return `${num1} ${operator} ${num2} = ${operation}`;
+    if (num1 === 0 && num2 === 0 && operator === '÷') {
+        display.textContent += `= y tho?`;
+    } else {
+    display.textContent += `= ${operation}`;
+    console.log(operation);
+    }
 }
 
-const buttons = document.querySelectorAll('button')
+function updateNums(button) {
+    if (display.firstChild === dummyText) {
+        display.removeChild(dummyText);
+    }
+    if (operator === '') {
+        num1 += +button.textContent;
+        display.textContent += button.textContent;
 
-let userSelectedKey = '';
+        console.log(num1);
+    } else if (operator !== '') {
+        num2 += +button.textContent;
+        display.textContent += button.textContent;
+
+        console.log(num2);
+    }
+}
+
+function updateOperator(button) {
+    if (display.firstChild !== dummyText &&
+        operator === '') {
+        operator = button.textContent;
+        display.textContent += button.textContent;
+
+        console.log(operator);
+    }
+}
+
+let num1 = 0;
+let operator = '';
+let num2 = 0;
+
 const display = document.querySelector('#displayOutput');
 
-const dummyText = document.createElement('p')
-dummyText.textContent = '0'
-
+const dummyText = document.createElement('p');
+dummyText.textContent = '0';
 display.appendChild(dummyText);
+
+const buttons = document.querySelectorAll('button')
 
 buttons.forEach((button) => {
     if (button.textContent !== 'Clear') {
         button.addEventListener('click', () => {
-            if (display.firstChild === dummyText) {
-                display.removeChild(dummyText);
-            };
-
-            userSelectedKey = button.textContent;
-
-            display.textContent += userSelectedKey;
+            if (button.classList.contains('number')) {
+                return updateNums(button);
+            } else if (button.classList.contains('operator') &&
+                button.id !== 'equals') {
+                return updateOperator(button)
+            } else if (button.id === 'equals') {
+                return operate();
+            }
         })
     }
 });
@@ -64,6 +92,9 @@ const clearBtn = document.querySelector('#clear');
 
 clearBtn.addEventListener('click', () => {
     display.textContent = '';
+    num1 = 0;
+    num2 = 0;
+    operator = '';
     if (display.firstChild !== dummyText) {
         display.appendChild(dummyText);
     }
